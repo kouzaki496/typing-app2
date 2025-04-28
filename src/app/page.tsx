cddf8a22098ae762d7990db9ccca55301a5c9212
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-04-28 13:50:13 +0900

    Buttonの表示確認

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind!</h1>
+      <Button>Click me</Button>
     </main>
   )
 }
