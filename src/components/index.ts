a2e0fb3fe76b66bda401a5efebb40eeab5c437a0
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-04-29 20:26:18 +0900

    import文の修正

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,3 @@
 export { Button, buttonVariants } from '@/components/ui/button';
 export { default as Layout } from '@/components/layout';
-export { default as Header } from '@/components/Header';
+export { default as Header } from '@/components/header';
