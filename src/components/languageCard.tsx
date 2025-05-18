217439bfb43495fbff4d3e81abc2e2fa4933839f
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-05-18 17:54:28 +0900

    選択時にハイライトされるように変更

--- src/components/languageCard.tsx
+++ src/components/languageCard.tsx
@@ -1,17 +1,24 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/";
 import { ReactNode } from "react";
+import { cn } from "@/utils/utils";
 
 
 type LanguageCardProps = {
   icon: ReactNode;
   name: string;
-  selected?: boolean;
+  selected: boolean;
   onClick: () => void;
 }
 
-export default function LanguageCard({ icon, name, onClick }: LanguageCardProps) {
+export default function LanguageCard({ icon, name, selected, onClick }: LanguageCardProps) {
   return (
-    <Card onClick={onClick}  className="cursor-pointer hover:shadow-lg transition py-2">
+    <Card
+      onClick={onClick}
+      className={cn(
+        "cursor-pointer transition border-2 py-2",
+        selected ? "border-primary bg-muted" : "border-transparent"
+      )}
+    >
       <CardContent className="flex items-center gap-4 p-4">
         <div className="text-2xl">{icon}</div>
         <div className="text-lg font-medium">{name}</div>
