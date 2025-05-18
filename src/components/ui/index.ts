a63f4c06290e0039bcc7fa40ae48732de4abdc1c
DESKTOP-04O64RR\kohzaki <yuki.kohzaki@gmail.com>
2025-05-18 12:03:37 +0900

    dialogのインポート

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,3 @@
 export { Button, buttonVariants } from '@/components/ui/button';
 export { Switch } from '@/components/ui/switch';
+export { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
